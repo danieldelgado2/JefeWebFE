b8b50fc4adbba0fa0fce4434795a5a6fc921f997
Laripoli <alaracazorla@gmail.com>
2021-01-19 00:31:08 +0100

    Mas cosa

--- TallerFE/js/index.js
+++ TallerFE/js/index.js
@@ -1,32 +1,48 @@
-$(document).ready(function(){
+const Index = function(){
 
+    const handler = function (json) {
+        _login();
+    };
 
-    $("#acceder").click(function(e){
-        // e.preventDefault();
+    const _login = function(){
 
-        var username = $("#username").val().trim();
-        var password = $("#password").val().trim();
+        $("#acceder").click(function(e){
+            // e.preventDefault();
+    
+            const $username = $("#username").val().trim();
+            const $password = $("#password").val().trim();
+            const $error = $("#error");
+            const $errorbd = $("#errorbd");
+            const $empty = $("#campos_vacios");
+    
+            if( $username != "" && $password != "" ){
+                $.ajax({
+                    url: 'https://localhost:44349/login',
+                    dataType: 'json',
+                    type: 'post',
+                    contentType: 'application/json',
+                    data: JSON.stringify({login:$username,password:$password}),
+                    success: function(data, status){
+                        //alert("Data: " + data + "\nStatus: " + status);
+                        if(data){ 
+                            window.location.href = "resumen_ventas.html";
+                        } else {
+                            $error.css("display","block");
+                        }
+                        
+                    }                
+                }).fail( function( jqXHR, textStatus, errorThrown ) {
+                    $errorbd.css("display","block");
+                });
+            }else {
+                $empty.css("display","block");
+            }
+        });
+    }
 
-        if( username != "" && password != "" ){
-            $.ajax({
-                url: 'https://localhost:44349/login',
-                dataType: 'json',
-                type: 'post',
-                contentType: 'application/json',
-                data: JSON.stringify({login:username,password:password}),
-                success: function(data, status){
-                    //alert("Data: " + data + "\nStatus: " + status);
-                    if(data){ 
-                        window.location.href = "resumen_ventas.html";
-                    } else {
-                        $("#error").css("display","block");
-                    }
-                }                
-            }).fail( function( jqXHR, textStatus, errorThrown ) {
-                $("#errorbd").css("display","block");
-            });
-        }else {
-            $('#campos_vacios').css("display","block");
-        }
-    });
-});
+    return {
+        init: function () {
+            handler();
+        },
+    };
+}();
--- TallerFE/js/resumen_ventas.js
+++ TallerFE/js/resumen_ventas.js
@@ -1,9 +1,13 @@
-/*
-Please consider that the JS part isn't production ready at all, I just code it to show the concept of merging filters and titles together !
-*/
-$(document).ready(function(){
-    
-    var tablaCheck = function(){
+var Ventas = function(){
+
+    var handler = function (json) {
+        _listado();
+        _limpiarFiltros();
+        _controlFiltros();
+    };
+
+    var _tablaCheck = function(){
+
         if($('.columnita').length == 0){
             $("#no-result").css("display","block");
         }else{
@@ -11,93 +15,85 @@ $(document).ready(function(){
         }
     }
 
+    var _listado = function(){
 
-    $.ajax({
-        url: 'https://localhost:44349/venta',
-        dataType: 'json',
-        type: 'get',
-        contentType: 'application/json',
-        success: function(data, status){
-            //Por cada elemento dentro del array data, construye una fila (tr)
-            //y añade celdas con los campos de cada elemento del array.
-		    $.each(data, function(i, item) {
-		       
-                
-                    var $div = $('<div class="row resultadito">').append(
-                        // $('<div class="col m-2">').text(item.login),
-                        $('<div class="col m-2 columnita">').text(item.nombre),
-                        $('<div class="col m-2 columnita">').text(item.apellidos),
-                        $('<div class="col m-2 columnita">').text(item.email),
-                        $('<div class="">'),
-                        $('<div class="col m-2 columnita">').text(item.importe + "€"),
-                        $('<div class="col m-2 columnita">') 
-                        ); 
-                        
-                        $('.resultados').append($div);
-                        
-                
-        
-                });
-                tablaCheck();
+        $.ajax({
+            url: 'https://localhost:44349/venta',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            success: function(data, status){
+                //Por cada elemento dentro del array data, varruye una fila (tr)
+                //y añade celdas con los campos de cada elemento del array.
+                $.each(data, function(i, item) {
+                        var $div = $('<div class="row resultadito">').append(
+                            // $('<div class="col m-2">').text(item.login),
+                            $('<div class="col m-2 columnita">').text(item.nombre),
+                            $('<div class="col m-2 columnita">').text(item.apellidos),
+                            $('<div class="col m-2 columnita">').text(item.email),
+                            $('<div class="">'),
+                            $('<div class="col m-2 columnita">').text(item.importe + "€"),
+                           $('<div class="col m-2 columnita">') 
+                            ); 
+                            $('.resultados').append($div);
+                    });
+                    _tablaCheck();
+                }
+            }).fail( function( jqXHR, textStatus, errorThrown ) {
+                $("#errorbd").css("display","block"); 
+            });
         }
-    }).fail( function( jqXHR, textStatus, errorThrown ) {
-        $("#errorbd").css("display","block");
-    });
-
-    $('#limpiar_filtros').on('click',function(){
-
-       $('input').val('');
-       $('.filterable .filters input').trigger('keyup'); 
         
-    });
+    var _limpiarFiltros = function(){
+        var $filter = $('#limpiar_filtros');
+        var $input = $('input');
 
+        $filter.on('click',function(){
 
+            $input.val('').trigger('keyup');
+             
+         });
+    }
 
-
-    $('.filterable .btn-filter').click(function(){
-        var $panel = $(this).parents('.filterable'),
-        $filters = $panel.find('.filters input'),
-        $tbody = $panel.find('.tabla tbody');
-        if ($filters.prop('disabled') == true) {
-            $filters.prop('disabled', false);
-            $filters.first().focus();
-        } else {
-            $filters.val('').prop('disabled', true);
-            $tbody.find('.no-result').remove();
-            $tbody.find('tr').show();
-        }
-    });
-
-    $('.filterable .filters input').keyup(function(e){
-        /* Ignore tab key */
-        var code = e.keyCode || e.which;
-        if (code == '9') return;
-        /* Useful DOM data and selectors */
-        var $input = $(this),
-        inputContent = $input.val().toLowerCase(),
-        $panel = $input.parents('.filterable'),
-        column = $panel.find('.filters div').index($input.parents('div')),
-        $tabla = $panel.find('.resultados'),
-        $rows = $tabla.find('.resultadito');
-        /* Dirtiest filter function ever ;) */
-        var $filteredRows = $rows.filter(function(){
-            var value = $(this).find('.columnita').eq(column).text().toLowerCase();
-            return value.indexOf(inputContent) === -1;
+    var _controlFiltros = function(){
+        $('.filterable .filters input').keyup(function(e){
+            /* Ignore tab key */
+            var code = e.keyCode || e.which;
+            if (code == '9') return;
+            /* Useful DOM data and selectors */
+            var $input = $(this),
+            inputContent = $input.val().toLowerCase(),
+            $panel = $input.parents('.filterable'),
+            column = $panel.find('.filters div').index($input.parents('div')),
+            $tabla = $panel.find('.resultados'),
+            $rows = $tabla.find('.resultadito');
+            /* Dirtiest filter function ever ;) */
+            var $filteredRows = $rows.filter(function(){
+                var value = $(this).find('.columnita').eq(column).text().toLowerCase();
+                return value.indexOf(inputContent) === -1;
+            });
+            /* Clean previous no-result if exist */
+            $tabla.find('.no-result').remove();
+            /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+            $rows.show();
+            $filteredRows.hide();
+            /* Prepend no-result row if all rows are filtered */
+            if ($filteredRows.length === $rows.length) {
+                $("#no-result").css("display","block");
+            }else{
+                $("#no-result").css("display","none");
+            }
         });
-        /* Clean previous no-result if exist */
-        $tabla.find('.no-result').remove();
-        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
-        $rows.show();
-        $filteredRows.hide();
-        /* Prepend no-result row if all rows are filtered */
-        if ($filteredRows.length === $rows.length) {
-            $("#no-result").css("display","block");
-        }else{
-            $("#no-result").css("display","none");
-        }
-    });
+    }
 
-    
-    
-    tablaCheck();
-});
+    return {
+        init: function () {
+            handler();
+        },
+    };
+}();
+
+
+
+
+
